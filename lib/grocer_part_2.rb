da8e621fa56de9c1228c4bc73cd0931b7df4beaa
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-13 23:56:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,7 +1,7 @@
 require_relative './part_1_solution.rb'
 def apply_coupons(cart, coupons)
   counter = 0 
-  while counter < coupons.length do 
+  while counter < coupons.length do
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
     item_with_coupon = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(item_with_coupon,item)
