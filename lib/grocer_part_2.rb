b3f6f5f78fb3108d58c12f07e6f9d0d4c7f451d1
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-13 23:41:56 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -15,8 +15,11 @@ def apply_coupons(cart, coupons)
       if cart_item_with_coupon 
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
+        
+      end 
       
-    end  counter += 1 
+    end 
+    counter += 1 
   end 
 end
 
