fb765e20e4132c9b078b37b2cd67b86cc34816c8
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-13 23:52:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -16,7 +16,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => item_with_coupon,
           :price => coupons[counter][:cost] / coupons[counter][:num],
-          :count => coupons[counter][:num]
+          :count => coupons[counter][:num],
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
