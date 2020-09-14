afaa896aefe1236f67667399cc44e8b2947535c3
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 00:45:27 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -39,9 +39,9 @@ end
 
 
 def checkout(cart, coupons)
-new_cart = consolidate_cart(cart)
-coupons_applied = apply_coupons(consolidate_cart, coupons)
- clearance_cart = apply_clearance(applied_coupons)
+  new_cart = consolidate_cart(cart)
+  coupons_applied = apply_coupons(consolidate_cart, coupons)
+ clearance_cart = apply_clearance(coupons_applied)
  total = 0 
  counter = 0 
  while counter < clearance_cart.length do 
