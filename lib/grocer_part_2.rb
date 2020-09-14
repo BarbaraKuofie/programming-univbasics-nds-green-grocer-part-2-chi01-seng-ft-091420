bcae1c426a49063888039ccc8b17c40acb310fdb
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 00:50:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -42,7 +42,7 @@ end
 def checkout(cart, coupons)
   new_cart = consolidate_cart(cart)
   coupons_applied = apply_coupons(new_cart, coupons)
- clearance_cart = apply_clearance(coupons_applied)
+  clearance_cart = apply_clearance(coupons_applied)
  binding.pry 
  total = 0 
  counter = 0 
