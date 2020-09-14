77d67070a24deb017bda247ffad58affacfa3c8e
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 00:06:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -26,9 +26,14 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ counter = 0 
+ discounted_cart = []
+ while counter < cart.length do 
+   if cart[counter][:clearance] == true 
+     discounted_cart = 
+ end 
+   counter += 1 
+ end 
 end
 
 def checkout(cart, coupons)
